0f232e9cf416cb9a6d60c078a76bc1bac22d3144
Ilnur Fattakhov <nurich1995@mail.ru>
2023-03-18 03:25:00 +0300

    add Corner on each view and button

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLabel: UIView!
+    @IBOutlet weak var yellowLabel: UIView!
+    @IBOutlet weak var greenLabel: UIView!
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeButton.layer.cornerRadius = 10
+        redLabel.layer.cornerRadius = 65
+        yellowLabel.layer.cornerRadius = 65
+        greenLabel.layer.cornerRadius = 65
     }
 
-
+    
+    @IBAction func changeColorButton() {
+//        changeButton.setTitle("NEXT", for: .normal)
+//        redLabel.alpha = 1
+//        if yellowLabel.alpha == 0.3 {
+//            redLabel.alpha = 0
+//            yellowLabel.alpha = 1
+//        } else {
+//            redLabel.alpha = 0.3
+//            yellowLabel.alpha = 0.3
+//            greenLabel.alpha = 1
+//        }
+    }
 }
 
