37afb9d990ce5a0e2b38c41ec502b95cff2fc8d5
Ilnur Fattakhov <nurich1995@mail.ru>
2023-03-19 16:03:31 +0300

    Totally change the liogic of application (viewController)

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -9,33 +9,46 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    
     @IBOutlet weak var redLabel: UIView!
     @IBOutlet weak var yellowLabel: UIView!
     @IBOutlet weak var greenLabel: UIView!
     @IBOutlet weak var changeButton: UIButton!
+
+    private enum Color {
+        case red
+        case yellow
+        case green
+    }
+    
+    private var color: Color = .red
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         changeButton.layer.cornerRadius = 10
         redLabel.layer.cornerRadius = 65
         yellowLabel.layer.cornerRadius = 65
         greenLabel.layer.cornerRadius = 65
+        
     }
     
     @IBAction func changeColorButton() {
         changeButton.setTitle("NEXT", for: .normal)
         
-        if redLabel.alpha == 0.5 {
+        switch color {
+        case .red:
             redLabel.alpha = 1
-        } else if redLabel.alpha == 1 {
+            greenLabel.alpha = 0.3
+            color = .yellow
+        case .yellow:
             redLabel.alpha = 0.3
             yellowLabel.alpha = 1
-        } else if yellowLabel.alpha == 1 {
+            color = .green
+        case .green:
             yellowLabel.alpha = 0.3
             greenLabel.alpha = 1
-        } else if greenLabel.alpha == 1 {
-            greenLabel.alpha = 0.3
-            redLabel.alpha = 1
+            color = .red
         }
     }
 }
